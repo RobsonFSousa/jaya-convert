5b646a94e960cb4fb99f2b8cb03b670d4b50a727
Robson Sousa <nosbor.ferreira@gmail.com>
2021-07-20 21:50:11 -0300

    Criando rotas e controlador de conversão

---
+++ src/main/kotlin/controller/ConversionController.kt
@@ -0,0 +1,15 @@
+package controller
+
+import io.javalin.http.Context
+import java.util.*
+
+
+object ConversionController {
+    fun convert(ctx: Context) {
+
+    }
+
+    fun listAll(ctx: Context) {
+
+    }
+}
